8f69f4488cd9966fa37c3a9876e6fb17c22ed6e1
fxpw <84588274+fxpw@users.noreply.github.com>
2021-07-20 00:14:08 +0300

    localisation

---
+++ SexyCooldown/Localization/ruRU.lua
@@ -0,0 +1,197 @@
+-- This file is none script-generated and should  be manually edited. 
+-- Localizers may copy this file to edit as necessary. 
+
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0") 
+local L = AceLocale:NewLocale("SexyCooldown", "ruRU", ["Бары"]) 
+if not L then return end 
+
+ 
+-- ./core.lua
+L["Select an options sub-category to get started."] = "Выберите подкатегорию опций, чтобы начать"
+L["Bars"] = "Бары"
+L["Create new bar"] = "Создать новый бар"
+L["Profiles"] = "Профили"
+L["Reset to defaults"] = "Восстановить"
+L["Reset all colors in this group to the icon defaults"] = ["Сбросить все цвета в этой группе на значения по умолчанию для значков"
+
+-- ./loadstub.lua
+-- no localization
+
+-- ./Modules/Auras.lua
+L["All buffs on me"] = "Все баффы на мне"
+L["Show the duration of buffs on me on this bar"] = "Показывает длительность баффов на мне на этой панели"
+L["All debuffs on me"] = "Все дебаффы на мне"
+L["Show the duration of my debuffs on me on this bar"] = "Показывает длительность моих дебаффов на мне на этой панели"
+L["My buffs on me"] = "Мои баффы на мне"
+L["Show the duration of my buffs on me on this bar"] = "Показывает продолжительность действия моих баффов на этой панели"
+L["My debuffs on me"] = "Мои дебаффы на мне"
+L["Show the duration of debuffs on me on this bar"] = "Показывать длительность дебаффов на мне на этой панели"
+L["My focus buffs"] = "Мои бафы фокуса"
+L["Show the duration of my buffs on my focus on this bar"] = "Показывает длительность баффов по фокусу на этой панели"
+L["My focus debuffs"] = "Мои дебафы фокуса"
+L["Show the duration of my debuffs on my focus on this bar"] = "Показывает длительность дебаффов по фокусу на этой панели"
+L["My target buffs"] = "Мои целевые баффы"
+L["Show the duration of my buffs on my target on this bar"] = "Показает продолжительность действия моих баффов на моей цели на этой панели"
+L["My target debuffs"] = "Мои целевые дебаффы"
+L["Show the duration of my debuffs on my target on this bar"] = "Показывает длительность моих дебаффов на моей цели на этой панели"
+L["All target buffs"] = "Все целевые баффы"
+L["Show the duration of all buffs on my target on this bar"] = "Показывает длительность всех баффов на моей цели на этой панели"
+L["All target debuffs"] =["Все целевые дебаффы"
+L["Show the duration of all debuffs on my target on this bar"] = "Показывает длительность всех дебаффов на моей цели на этой панели"
+
+-- ./Modules/Items.lua
+L["Items"] = "Предметы"
+L["Show my item cooldowns on this bar"] = "Показывать время восстановления моих предметов на этой панели"
+L["Internal cooldowns"] = "Внутренние кулдауны"
+L["Show internal cooldowns for my items and enchants on this bar."] = "Показывать внутренние кулдауны для моих предметов и чар на этой панели."
+
+-- ./Modules/RaidDebuffs.lua
+L["Major Armor Debuff"] = " Ослабление брони"
+L["Attack Speed Debuff"] = "Снижение скорости атаки"
+L["Bleed Damage"] = "Кровотечение"
+L["Cast Speed Debuff"] = "Снижение скорости сотворения чар"
+L["Healing Debuff"] = "Исцеляющий дебафф"
+L["Physical Damage Reduction"] = "Снижение физического урона"
+L["Physical Vulnerability"] = "Физическая уязвимость"
+L["Spell Crit Chance"] = "Шанс критического удара заклинанием"
+L["Spell Damage Taken"] = "Полученный урон от заклинаний"
+L["Major Armor Debuffs"] = "Ослабления брони"
+L["Show the duration of major armor debuffs on the target"] = "Показать длительность основных дебаффов брони на цели"
+L["Attack Speed Debuffs"] = "Снижение скорости атаки"
+L["Show the duration of attack speed debuffs on the target"] = "Показывать длительность дебаффов скорости атаки на цели"
+L["Bleed Damage Debuffs"] = "Кровотечение дебафы"
+L["Show the duration of bleed damage debuffs on the target"] = "Показать длительность дебаффов кровотечения на цели"
+L["Cast Speed Debuffs"] = "Ослабления скорости сотворения чар"
+L["Show the duration of cast speed debuffs on the target"] = "Покаpfnm продолжительность дебаффов скорости каста на цели"
+L["Show the duration of -healing debuffs on the target"] = ["Бары"]
+L["Physical Damage Reduction Debuffs"] = ["Бары"]
+L["Show the duration of physical damage reduction debuffs on the target"] = ["Бары"]
+L["Physical Vulnerability Debuffs"] = ["Бары"]
+L["Show the duration of physical vulnerability debuffs on the target"] = ["Бары"]
+L["Spell Crit Debuffs"] = ["Бары"]
+L["Show the duration of spell crit chance debuffs on the target"] = ["Бары"]
+L["Spell Damage Debuffs"] = ["Бары"]
+L["Show the duration of spell damage debuffs on the target"] = ["Бары"]
+
+-- ./Modules/Spells.lua
+L["My spells"] = ["Бары"]
+L["Show my spell cooldowns on this bar"] = ["Бары"]
+L["Pet spells"] = ["Бары"]
+L["Show my pet's cooldowns on this bar"] = ["Бары"]
+L["Show internal cooldowns for my spells and talents on this bar."] = ["Бары"]
+
+-- ./Modules/Totems.lua
+L["Totem durations"] = ["Бары"]
+L["Show my totem durations on this bar"] = ["Бары"]
+
+-- ./options.lua
+L["No outline"] = ["Бары"]
+L["Outline"] = ["Бары"]
+L["Thick Outline"] = ["Бары"]
+L["Left to Right"] = ["Бары"]
+L["Right to Left"] = ["Бары"]
+L["Top to Bottom"] = ["Бары"]
+L["Bottom to top"] = ["Бары"]
+L["Center left"] = ["Бары"]
+L["Center right"] = ["Бары"]
+L["Center"] = ["Бары"]
+L["Top left"] = ["Бары"]
+L["Top right"] = ["Бары"]
+L["Top center"] = ["Бары"]
+L["Bottom left"] = ["Бары"]
+L["Bottom center"] = ["Бары"]
+L["Bottom right"] = ["Бары"]
+L["Clone from..."] = ["Бары"]
+L["Clone from another bar"] = ["Бары"]
+L["Icons"] = ["Бары"]
+L["Right-click an icon on a cooldown bar to blacklist it and prevent it from showing in the future. To remove a cooldown from a blacklist, choose the cooldown to remove from this list."] = ["Бары"]
+L["Blacklist"] = ["Бары"]
+L["Select an item to remove it from the blacklist."] = ["Бары"]
+L["Removed %s from the SexyCooldown blacklist"] = ["Бары"]
+L["Clamp to screen"] = ["Бары"]
+L["Prevent icons from running off the screen"] = ["Бары"]
+L["Cooldown Text"] = ["Бары"]
+L["Show Cooldown Text"] = ["Бары"]
+L["Show Stacks"] = ["Бары"]
+L["Show the number of stacks of a buff or debuff"] = ["Бары"]
+L["Time corner"] = ["Бары"]
+L["Select the corner to anchor the time text to."] = ["Бары"]
+L["Stacks corner"] = ["Бары"]
+L["Select the corner to anchor the stack text to."] = ["Бары"]
+L["Stacks offset"] = ["Бары"]
+L["Offset for stack text"] = ["Бары"]
+L["Time offset"] = ["Бары"]
+L["Offset for time text"] = ["Бары"]
+L["Font"] = ["Бары"]
+L["Font size"] = ["Бары"]
+L["Font color"] = ["Бары"]
+L["Font Outline"] = ["Бары"]
+L["Borders"] = ["Бары"]
+L["Border"] = ["Бары"]
+L["Border color"] = ["Бары"]
+L["Border size"] = ["Бары"]
+L["Border inset"] = ["Бары"]
+L["General options"] = ["Бары"]
+L["Icon size"] = ["Бары"]
+L["How big to make icons, relative to their parent bar"] = ["Бары"]
+L["Finish Behavior"] = ["Бары"]
+L["Splash scale"] = ["Бары"]
+L["How big (or small) icons will 'splash' when their cooldown is done"] = ["Бары"]
+L["Splash speed"] = ["Бары"]
+L["How quickly to play the splash animation once a cooldown is done"] = ["Бары"]
+L["Disable tooltips"] = ["Бары"]
+L["Don't show item or spell tooltips when you hover over their icons on the bar."] = ["Бары"]
+L["Disable mouse"] = ["Бары"]
+L["Don't allow mouse interaction with icons. If you turn this off, you won't be able to blacklist items."] = ["Бары"]
+L["Pulse speed"] = ["Бары"]
+L["Speed to pulse overlapping icons"] = ["Бары"]
+L["Bar"] = ["Бары"]
+L["General Options"] = ["Бары"]
+L["Name"] = ["Бары"]
+L["A descriptive name for this bar"] = ["Бары"]
+L["Lock"] = ["Бары"]
+L["Lock this bar to prevent resizing or moving"] = ["Бары"]
+L["Flexible"] = ["Бары"]
+L["Collapse the bar to the length of your longest active cooldown."] = ["Бары"]
+L["Advanced Options"] = ["Бары"]
+L["Enable advanced configuration options"] = ["Бары"]
+L["Bar Orientation"] = ["Бары"]
+L["Bar and icon direction."] = ["Бары"]
+L["Time and Duration settings"] = ["Бары"]
+L["Time Compression"] = ["Бары"]
+L["Time display scaling factor"] = ["Бары"]
+L["Max Time"] = ["Бары"]
+L["Max time to display, in seconds"] = ["Бары"]
+L["Minimum duration"] = ["Бары"]
+L["Cooldowns shorter than this will not be shown."] = ["Бары"]
+L["Maximum duration"] = ["Бары"]
+L["Cooldowns longer than this will not be shown. Set to 0 to show all cooldowns."] = ["Бары"]
+L["Border and Background"] = ["Бары"]
+L["Background"] = ["Бары"]
+L["Background color"] = ["Бары"]
+L["Border insets"] = ["Бары"]
+L["Position and Size"] = ["Бары"]
+L["Height"] = ["Бары"]
+L["Width"] = ["Бары"]
+L["Horizontal Position"] = ["Бары"]
+L["Horizontal offset from screen center"] = ["Бары"]
+L["Vertical Offset"] = ["Бары"]
+L["Vertical offset from screen center"] = ["Бары"]
+L["Inactive Opacity"] = ["Бары"]
+L["Opacity to set the bar to when a cooldown is not active"] = ["Бары"]
+L["Show Splash Anchor"] = ["Бары"]
+L["Show an anchor that you can use to move the splash anchor, which controls where icons 'splash' when they are cooled down."] = ["Бары"]
+L["Reset Splash Anchor"] = ["Бары"]
+L["Reset splash anchor to its default position."] = ["Бары"]
+L["/!\\ Delete This Bar /!\\"] = ["Бары"]
+L["Permanently delete this bar."] = ["Бары"]
+L["Are you sure you want to delete this bar?"] = ["Бары"]
+L["Custom timings"] = ["Бары"]
+L["Specify a comma-delimited list of intervals to mark on the bar, in seconds. For example: 1,5,10,30,45,60,180"] = ["Бары"]
+L["Timings format"] = ["Бары"]
+L["Select the display format for timings"] = ["Бары"]
+L["Event Types"] = ["Бары"]
+L["Event Colors"] = ["Бары"]
+
+-- ./prototype.lua
+-- no localization
